f7d4df447d1be9bfc726c785e72434efd054630c
Ryan Chen <ryanchen@spokeo.com>
2013-06-13 20:05:56 -0700

    Add readme file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '283779c332f797039a968eadd65e2d936eeac0284e0f4948b2eabe8d27cb77de62a159a4b22c2049a5d76a8a572084eab5c5e4c5f53452b7bc646cb2853d0269'
